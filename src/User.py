a3aa6096f79317b409e9dc714c05c35ec6d4633a
Vaibhav Sharma <yavsvaibhav@gmail.com>
2021-03-06 09:51:10 +0530

    V2

--- src/User.py
+++ src/User.py
@@ -1,6 +1,7 @@
 import main
 import pandas as pd
-import random
+import os.path
+
 
 class AvailableFlights:
 
@@ -11,6 +12,7 @@ class AvailableFlights:
         self.startingPoint = starting
         self.destination = destination
         self.paymentMethod = payment
+        self.choosenFlight = None
 
     def getAllFlights(self):
         self.allFlights = main.requiredDf.loc[(main.requiredDf.ORIGIN_AIRPORT == self.startingPoint) &
@@ -23,6 +25,7 @@ class AvailableFlights:
         self.choosenFlight = self.allFlights.loc[(self.allFlights.AIRLINE == self.airlineName) &
                                             (self.allFlights.FLIGHT_NUMBER == self.airlineNumber)]
         return self.choosenFlight
+
 
 class Distance(AvailableFlights):
 
@@ -45,7 +48,6 @@ class Payment(Distance):
         cost = self.distance * 5
         return cost
 
-
     def transactionProcess(self):
         if self.paymentMethod == "PayTM":
             number = int(input("Enter your UPI ID"))
@@ -55,6 +57,8 @@ class Payment(Distance):
             self.transactionStatus = True
         else:
             self.transactionStatus = False
+        return "Complete"
+
 
 class User(Payment):
     def __init__(self, first, last, email, starting, destination, payment):
@@ -66,15 +70,25 @@ class User(Payment):
             self.seatBooked = True
         else:
             self.seatBooked = False
+        return 'Seat Booked'
 
     def setSeatNumber(self, flightClass, preference):
-        frame = pd.DataFrame(None, [n for n in range(1, 24, 1)], ['A', 'B', 'C'])
-        requiredFrame = frame.loc[[n for n in range(1, 24)], 'A']
+
+        path_to_requiredFrame = '{}{}.csv'.format(self.choosenFlight.at[0, 'AIRLINE'], self.choosenFlight.at[0, 'FLIGHT_NUMBER'])
+        try:
+            x = open(path_to_requiredFrame)
+            frame = pd.read_csv(path_to_requiredFrame)
+        except IOError:
+            frame = pd.DataFrame(None, [n for n in range(1, 24, 1)], ['A', 'B', 'C'])
+            frame.to_csv(path_to_requiredFrame, index = False)
+        requiredFrame = frame.loc[:, "A"]
+
         if preference.lower() == "window":
             allTaken = 0
             for i in range(1, 24):
-                if pd.isnull(requiredFrame[i])
-                    requiredFrame[i] = "Taken"
+                if pd.isnull(requiredFrame[i]):
+                    requiredFrame[i] = self.firstName
+                    frame.to_csv(path_to_requiredFrame, index = False)
                     break
                 else:
                     allTaken += 1
@@ -82,6 +96,33 @@ class User(Payment):
                 print("Sorry but all the window seats are taken")
             else:
                 print("All done")
+        try:
+            requiredFrame = open(path_to_requiredFrame)
+            frame = pd.read_csv(path_to_requiredFrame)
+        except IOError:
+            frame = pd.DataFrame(None, [n for n in range(1, 24, 1)], ['A', 'B', 'C'])
+            frame.to_csv(path_to_requiredFrame, index = False)
+        requiredFrame1 = frame.loc[:, "B"]
+        requiredFrame2 = frame.loc[:, "C"]
+
+        if preference.lower() == 'aisle':
+            BFilled = False
+            for i in range(0, 24):
+                if pd.isnull(requiredFrame1[i]):
+                    requiredFrame1.iloc[i] = self.firstName
+                    frame.to_csv(path_to_requiredFrame, index = False)
+                    break
+                else:
+                    BFilled = True
+            if BFilled == True:
+                for i in range(0, 24):
+                    if pd.isnull(requiredFrame2[i]):
+                        requiredFrame2.iloc[i] = self.firstName
+                        requiredFrame2.to_csv(path_to_requiredFrame)
+                        break
+
+
+
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import User
 
 data = pd.read_csv('../resources/flights.csv', nrows = 1000)
 df = pd.DataFrame(data)
 requiredDf = df.loc[:, ['YEAR', "DAY_OF_WEEK", "AIRLINE", "FLIGHT_NUMBER", "TAIL_NUMBER",
             "ORIGIN_AIRPORT", "DESTINATION_AIRPORT", "SCHEDULED_DEPARTURE",
             "AIR_TIME", "DISTANCE", "SCHEDULED_ARRIVAL"]]
+
+def main():
 
 
